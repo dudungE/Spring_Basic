940980785fe820238664c244e54d52ddf646886a
JaeHyun Jeong <75703922+dudungE@users.noreply.github.com>
2025-05-18 22:50:17 +0900

    feat: 관심사 분리

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,42 +1,29 @@
 package hello.core.member;
 
-import hello.core.member.Grade;
-import hello.core.member.Member;
-import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
+import hello.core.AppConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+class MemberServiceTest {
+    MemberService memberService;
 
-@DisplayName("MemberService 클래스의")
-public class MemberServiceTest {
-
-    private MemberService memberService = new MemberServiceImpl();
-
-    @DisplayName("join 메서드는")
-    @Nested
-    class Describe_join {
-        @DisplayName("만약 아이디기 중복되지 않은 멤버를 저장할 경우")
-        @Nested
-        class Context_with_saved_valid_member {
-            @DisplayName("회원 정보를 저장한다.")
-            @Test
-            void join() {
-                //given
-                Member member = new Member(1L, "memberA", Grade.VIP);
-
-                //when
-                memberService.join(member);
-                Member findMember = memberService.findMember(1L);
-
-                //then
-                assertThat(findMember).isEqualTo(findMember);
-            }
-        }
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
     }
 
+    @Test
+    void join() {
+        //given
+        Member member = new Member(1L, "memberA", Grade.VIP);
 
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
 
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
 }
